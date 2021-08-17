1df226df40bc09c32a0436e72deedafca89a9f7b
Lucas Rivaldo <lucasrivaldo.int@gmail.com>
2021-08-17 10:30:39 -0300

    initial commit

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate (new Vector3 (15.0f, 30.0f, 45.0f) * Time.deltaTime);
+    }
+}
